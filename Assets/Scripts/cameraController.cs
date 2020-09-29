172f53bbf012163434b877324ba45ad99e3ed0f4
Jacob Lueke <jacoblueke@gmail.com>
2020-09-29 11:14:58 +0100

    added walls and camera controll (bot fixed camerea rotation yet)

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class cameraController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    public float speed = 0;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void OnMove(InputValue movementValue)
     {
-        transform.position = player.transform.position = offset;
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
 }
