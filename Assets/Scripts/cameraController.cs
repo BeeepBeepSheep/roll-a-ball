936bf3ecf78b48bf5d94a6904fd31649c3deaf52
515291 <515291@chrome.escg.ac.uk>
2020-10-01 15:22:44 +0100

    first commit/ fixed all bugs from previus version

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -1,36 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class cameraController : MonoBehaviour
 {
-    public float speed = 0;
+    public GameObject player;
 
-    private Rigidbody rb;
-
-    private float movementX;
-    private float movementY;
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        offset = transform.position - player.transform.position;
     }
 
-    private void OnMove(InputValue movementValue)
+    // Update is called once per frame
+    void LateUpdate()
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        transform.position = player.transform.position + offset;
     }
-
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
-    }
-
 }
